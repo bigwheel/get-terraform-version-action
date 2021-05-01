d6f07f4fc0dcff17dcc74d8bbce8d2fe6ce5891f
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-05-01 17:09:13 +0900

    Implementing test

--- src/main.sh
+++ src/main.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
+
+target_path=$1
 
 tmpdir=$(mktemp -d)
 tfenv_path=$tmpdir/.tfenv
 
-git clone https://github.com/tfutils/tfenv.git $tfenv_path
+# https://github.com/tfutils/tfenv#manual
+git clone https://github.com/tfutils/tfenv.git $tfenv_path 2> /dev/null
+PATH="$tfenv_path/bin:$PATH"
+
+# by https://github.com/tfutils/tfenv/blob/43e0aad9/libexec/tfenv-install#L66-L75
+cd $target_path
+resolved="$(tfenv resolve-version)"
+regex="${resolved##*\:}"
+version="$(tfenv list-remote | grep -e "${regex}" | head -n 1)"
+
+echo $version
--- test/test.bats
+++ test/test.bats
@@ -1,10 +1,15 @@
 # https://bats-core.readthedocs.io/en/latest/tutorial.html#quick-installation
 setup() {
+    load 'test_helper/bats-support/load'
+    load 'test_helper/bats-assert/load'
     DIR="$( cd "$( dirname "$BATS_TEST_FILENAME" )" >/dev/null 2>&1 && pwd )"
     PATH="$DIR/../src:$PATH"
 }
 
 
-@test "can run our script" {
-  main.sh
+@test "simple" {
+    tmpdir=$(mktemp -d)
+    echo '0.11.7' > $tmpdir/.terraform-version
+    run main.sh $tmpdir
+    assert_output '0.11.7'
 }
