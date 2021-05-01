bf7e06237b069412e383e4f3763f0f0ba2757064
k.bigwheel <k.bigwheel+eng@gmail.com>
2021-05-01 13:57:00 +0900

    Developing main script

--- src/main.sh
+++ src/main.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+set -eux
+
+tmpdir=$(mktemp -d)
+tfenv_path=$tmpdir/.tfenv
+
+if [[ ! -e $tfenv_path ]]; then
+  git clone https://github.com/tfutils/tfenv.git $tfenv_path
+fi
+
 echo "Goodbye"
